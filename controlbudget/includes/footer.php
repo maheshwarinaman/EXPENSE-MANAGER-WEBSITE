5775867018679602fa6a9a36a09880d9098b429b
maheshwarinaman <49190119+maheshwarinaman@users.noreply.github.com>
2020-11-23 11:45:59 +0530

    c

---
+++ controlbudget/includes/footer.php
@@ -0,0 +1,9 @@
+<footer>    
+     <div class="container">        
+         <center>         
+             <p>Copyright &copy; Control Budget. All Rights Reserved|Contact Us: +91-7253904551, +91-7060183215 </p>     
+             
+         </center>  
+     </div> 
+ </footer>
+   
